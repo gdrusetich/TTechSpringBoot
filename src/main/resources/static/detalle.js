337ee0f43f6a37aa925e2866aaea3bac4f817122
gdrusetich <gdrusetich@frba.utn.edu.ar>
2026-03-24 13:25:40 -0300

    Corrigiendo rutas en home

--- src/main/resources/static/detalle.js
+++ src/main/resources/static/detalle.js
@@ -1,7 +1,12 @@
 const API_URL = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
     ? "http://localhost:8081" 
     : "https://dinastia-6gsq.onrender.com";
-const URL_IMAGES = "/images";
+const API_PRODUCTS = `${API_URL}/products`;
+const API_CATEGORIES = `${API_URL}/categories`;
+const FOLDER_UPLOADS = "/uploads";
+const FOLDER_SYSTEM = "/images";
+const rutaDefault = `${FOLDER_SYSTEM}/default.jpg`;
+const rutaWA = `${FOLDER_SYSTEM}/WhatsApp.png`;
 let campoActual = ''; 
 let urlActual = '';
 let productoActual = null;
